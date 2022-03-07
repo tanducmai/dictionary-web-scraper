0de4244da48c46641242b64ea9b2ee03def2fa89
Tan Duc Mai <tan.duc.work@gmail.com>
2022-03-07 23:41:39 +1030

    Change description

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Author:       Tan Duc Mai
 # Email:        tan.duc.work@gmail.com
 # Date:         20-Aug-21
-# Description:  The main working area of the dictionary.
+# Description:  Retrieve the content of an online dictionary.
 #   I hereby declare that I completed this work without any improper help
 #   from a third party and without using any aids other than those cited.
 #
