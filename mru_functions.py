31192f4cecf97aaf239ded66a926087dc30569da
Tan Duc Mai <tan.duc.work@gmail.com>
2022-03-07 11:44:53 +1030

    Reformat module

--- mru_functions.py
+++ mru_functions.py
@@ -1,28 +1,26 @@
-#!/usr/bin/python3
-
 #
 # File:         mru_functions.py
 # Author:       Tan Duc Mai
 # Email:        tan.duc.work@gmail.com
 # Date:         20-Aug-21
-# Description:  Define functions that help with code legibility and code reuse.
+# Description:  Define resuable functions that help with code legibility.
 #   I hereby declare that I completed this work without any improper help
 #   from a third party and without using any aids other than those cited.
 #
 
+
+# ------------------------------- Module Imports ------------------------------
 """
-The time module has the sleep() function which gives some break (0.5 to 1 second) between each part of the program.
+The time module has the sleep() function which is used to give a short break
+(0.5 to 1 second) between each major part of the program.
 
-The requests nodule allows for the exchange of HTTP requests.
-
-The bs4 module has the BeautifulSoup which downloads data of HTML files of the website. It requires pre-installation –> pip install bs4.
+The requests module allows for the exchange of HTTP requests.
 """
-
 import time
 import requests
-from bs4 import BeautifulSoup as bs
 
 
+# ---------------------------- Function Definitions ---------------------------
 # A function to draw a line break.
 def line():
     print('\n-----------------------------------------------------------------------\n')
@@ -32,14 +30,14 @@ def line():
 # A function to find all other  definitions of a  word.
 def find_all_definitions(count, definition):
     for i in range(2, count + 1):
-        print(f'Entry {i} of {count}', end = '')
+        print(f'Entry {i} of {count}', end='')
         if i == 2:
             i = definition.find_next('span', class_='dtText')
         else:
             i = j.find_next('span', class_='dtText')
         j = i
         if ': ' not in i.get_text():
-            print(': ', end = '')
+            print(': ', end='')
         print(i.get_text())
 
 
